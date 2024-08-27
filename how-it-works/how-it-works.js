2ccf71fd353b59434453d0e16bdaaed7a2980311
Alessandro <pitasi.alessandro@gmail.com>
2024-08-27 23:59:26 +0200

    finished comefunziona.html with responsiveness and added galleria.html page

--- how-it-works/how-it-works.js
+++
@@ -1,3 +0,0 @@
-function go_to_main() {
-    window.location.href = '../index.html#form'
-}
--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const prenota_link = document.getElementById('scroll-link')
+
 prenota_link.addEventListener ('click', () => {
     event.preventDefault;
     scrolldown();
 })
+
+function go_to_main() {
+    window.location.href = '../index.html#form'
+}
+
 function scrolldown() {
     window.scrollTo({
         top:window.innerHeight,
@@ -12,7 +18,6 @@ function scrolldown() {
 
 window.addEventListener('load', function() {
     const hash = window.location.hash;
-    console.log(hash)
     if (hash === '#form') {
         scrolldown()
     }
