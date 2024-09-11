e5213b9caf97b0e76a0a61cb4e06857ed9cecee5
Alessandro <pitasi.alessandro@gmail.com>
2024-09-12 00:55:51 +0200

    images optimization, removed contatti and come funziona and implemented them in main page

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-const prenota_link = document.getElementById('scroll-link')
-
-prenota_link.addEventListener ('click', () => {
-    event.preventDefault;
-    scrolldown();
+const contact_panel = document.getElementById('contact_panel')
+const observer = new IntersectionObserver( (entries) => {
+    entries.forEach( (entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        }
+        else {
+            entry.target.classList.remove('show')
+        }
+    })
 })
+const hidden_elements = document.querySelectorAll('.hidden')
+// tell the observer to observe all hidden elements
+hidden_elements.forEach( (el) => observer.observe(el))
 
 function go_to_main() {
     window.location.href = '../index.html#form'
 }
 
 function scrolldown() {
+    top_pos = contact_panel.offsetTop;
     window.scrollTo({
-        top:window.innerHeight,
+        top:top_pos - 60,
         behavior:"smooth"
     })
 }
